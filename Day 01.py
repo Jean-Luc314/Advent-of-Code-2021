0172f723dbb3a93819abeea874f3949586125d08
Jean-Luc Parker <jldegp@gmail.com>
2021-12-01 20:59:31 +0000

    Add files via upload
    Uses compose()

--- Day 01.py
+++ Day 01.py
@@ -1,19 +1,21 @@
-### Part 1
+### Functions
+from compose import compose
 didIncrease = lambda x, y : y > x
 countIncreases = lambda data : sum(list(map(didIncrease, data[0:len(data)-1], data[1:len(data)])))
 def read_data(path):
     measurements_str = list(open(path))
     measurements = list(map(lambda string : int(string.replace("\n", "")), measurements_str))
     return measurements
+sumTriples = lambda measurements : list(map(lambda i : sum(measurements[i:i + 3]), range(len(measurements) - 2)))
+
+### Part 1
 
 ## Example
 measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 countIncreases(measurements)
 
 ## Solution
-measurements = read_data("Inputs/Day 01.txt")
-countIncreases(measurements)
-
+compose(countIncreases, read_data)("Inputs/Day 01.txt")
 
 ### Part 2
 # Need to iterate over all triples, summing each one e.g.
@@ -24,12 +26,10 @@ countIncreases(measurements)
 #    sum(measurements[len(measurements)-3:len(measurements)])
 #]
 
-sumTriples = lambda measurements : list(map(lambda i : sum(measurements[i:i + 3]), range(len(measurements) - 2)))
 
 ## Example
 measurements = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
 countIncreases(sumTriples(measurements))
 
 ## Solution
-measurements = read_data("Inputs/Day 01.txt")
-countIncreases(sumTriples(measurements))
+compose(countIncreases, sumTriples, read_data)("Inputs/Day 01.txt")
