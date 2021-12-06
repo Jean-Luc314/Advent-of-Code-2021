1486f5aa11778aecb5b662cd8c932b7f358fda26
Jean-Luc Parker <jldegp@gmail.com>
2021-12-06 20:26:20 +0000

    Tidied Solution

--- Day 06.py
+++ Day 06.py
@@ -1,10 +1,48 @@
 ### Functions
 from itertools import chain
 from functools import reduce, partial
-## Part 1 Very inefficient
+
 def splitString(string, sep = ","):
     return string.split(sep)
 
+def countClocks(clock, timers):
+    return sum(map(lambda t : t == clock, timers))
+
+def iterateGeneration(fishClockCounts, flush, maxClock = 8, reCycle = 6):
+    fishRespawnCount = fishClockCounts[0]
+    fishClockCounts[reCycle + 1] += fishRespawnCount
+    fishClockCounts = fishClockCounts[1:maxClock + 1] + [fishRespawnCount]
+    return fishClockCounts
+
+def countFish(path, nDays, maxClock = 8, reCycle = 6):
+    fishTimers = list(map(int, splitString(list(open(path))[0])))
+    fishClocks = list(range(maxClock + 1))
+    fishClockCounts = list(map(partial(countClocks, timers = fishTimers), fishClocks))
+    days = list(range(nDays))
+    fishClockCounts = reduce(partial(iterateGeneration,
+                                     maxClock = maxClock,
+                                     reCycle = reCycle),
+                             [fishClockCounts] + days)
+    return sum(fishClockCounts)
+
+### Part 1
+## Example
+path = "Inputs/Day 06 Example.txt"
+countFish(path, 18) # 26
+countFish(path, 80) # 5934
+## Solution
+path = "Inputs/Day 06.txt"
+countFish(path, 80) # 359344
+### Part 2
+## Example
+path = "Inputs/Day 06 Example.txt"
+countFish(path, 256) # 26984457539
+
+## Solution
+path = "Inputs/Day 06.txt"
+countFish(path, 256) # 1629570219571
+
+## Part 1 Original (Very inefficient)
 class laternfish:
     timer = 8
 
@@ -22,9 +60,6 @@ def ageFish(fish):
     else:
         return fish
 
-def isNotNone(x):
-    return x is not None
-
 def totuple(var):
     if isinstance(var, tuple):
         return var
@@ -34,48 +69,9 @@ def totuple(var):
 def populateFish(fish, flush):
     return list(chain(*map(totuple, map(ageFish, fish))))
 
-def countFish(path, nDays):
+def countFishSlow(path, nDays):
     fishTimers = list(map(int, splitString(list(open(path))[0])))
     myFish = list(map(spawnFish, fishTimers))
-    days = list(range(nDays))
-    days = [myFish] + days
-    
-    myFish = reduce(populateFish, days)
+    days = list(range(nDays))    
+    myFish = reduce(populateFish, [myFish] + days)
     return len(myFish)
-## Part 2
-def countClocks(clock, timers):
-    return sum(map(lambda t : t == clock, timers))
-
-def iterateGeneration(fishClockCounts, flush, maxClock):
-    fishRespawnCount = fishClockCounts[0]
-    fishClockCounts[7] += fishRespawnCount
-    fishClockCounts = fishClockCounts[1:maxClock + 1] + [fishRespawnCount]
-    return fishClockCounts
-
-def countFishFast(path, nDays):
-    fishTimers = list(map(int, splitString(list(open(path))[0])))
-    maxClock = 8
-    fishClocks = list(range(maxClock + 1))
-    fishClockCounts = list(map(partial(countClocks, timers = fishTimers), fishClocks))
-    days = list(range(nDays))
-    days = [fishClockCounts] + days
-    
-    fishClockCounts = reduce(partial(iterateGeneration, maxClock = 8), days)
-    return sum(fishClockCounts)
-
-### Part 1
-## Example
-path = "Inputs/Day 06 Example.txt"
-countFish(path, 18) # 26
-countFish(path, 80) # 5934
-## Solution
-path = "Inputs/Day 06.txt"
-countFish(path, 80) # 359344
-### Part 2
-## Example
-path = "Inputs/Day 06 Example.txt"
-countFishFast(path, 256) # 26984457539
-
-## Solution
-path = "Inputs/Day 06.txt"
-countFishFast(path, 256) # 1629570219571
